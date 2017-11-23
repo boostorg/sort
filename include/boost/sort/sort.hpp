31d48b0d84d270c9f01284e9922dfca4f0433303
Orson Peters <orsonpeters@gmail.com>
2017-11-23 18:45:34 +0100

    Renamed pdqsort.h to pdqsort.hpp and added tests for pdqsort.

--- include/boost/sort/sort.hpp
+++ include/boost/sort/sort.hpp
@@ -16,7 +16,7 @@ Cumulative include for the Boost Sort library
 #include <boost/sort/spreadsort/spreadsort.hpp>
 #include <boost/sort/spinsort/spinsort.hpp>
 #include <boost/sort/flat_stable_sort/flat_stable_sort.hpp>
-#include <boost/sort/pdqsort/pdqsort.h>
+#include <boost/sort/pdqsort/pdqsort.hpp>
 #include <boost/sort/block_indirect_sort/block_indirect_sort.hpp>
 #include <boost/sort/sample_sort/sample_sort.hpp>
 #include <boost/sort/parallel_stable_sort/parallel_stable_sort.hpp>
